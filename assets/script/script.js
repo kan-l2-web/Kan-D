dd165735610ea4146ee8f90099dc403eaade4fc7
kan-l2-web <kandemirkarasal8@gmail.com>
2022-08-30 17:42:41 +0700

    fix icon

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,17 @@
+document.body.style.overflow = "hidden";
+
 document.body.onload = function () {
 
 	setTimeout(function() {
 		const preloader = document.querySelector('.preloader');
 
 		if (!preloader.classList.contains('done')) {
-			preloader.classList.add('done')
+			preloader.classList.add('done');
+
+			document.body.style.overflow = "auto";
 		}
+
+		setTimeout(() => {preloader.style.display = "none";}, 3000);
 	}, 1500);
 
 }
