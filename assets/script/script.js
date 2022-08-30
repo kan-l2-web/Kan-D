d708df7d05efabf7ac14f1808a868761b5d6ccb6
kan-l2-web <kandemirkarasal8@gmail.com>
2022-08-30 17:52:22 +0700

    fix icon and update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
-// Hello world
+document.body.style.overflow = "hidden";
+
+document.body.onload = function () {
+
+	setTimeout(function() {
+		const preloader = document.querySelector('.preloader');
+
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+
+			document.body.style.overflow = "auto";
+		}
+
+		setTimeout(() => {preloader.style.display = "none";}, 3000);
+	}, 1500);
+
+}
